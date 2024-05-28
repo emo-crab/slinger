1727eee08f834d1a638703c5a3d3bff0fd77c275
cn-kali-team <kali-team@qq.com>
2024-05-28 17:32:38 +0800

    update

--- examples/tcp_socket.rs
+++ examples/tcp_socket.rs
@@ -3,17 +3,37 @@ use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr};
 use std::time::Duration;
 
 fn main() {
+  ssh();
+  redis();
+}
+
+fn ssh() {
   let add = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 22);
   let timeout = Duration::new(3, 0);
-  let mut socket = slinger::ConnectorBuilder::default()
-    .connect_timeout(Some(timeout))
-    .read_timeout(Some(timeout))
-    .write_timeout(Some(timeout))
-    .build()
-    .unwrap()
-    .connect_with_addr(add)
-    .unwrap();
+  let mut socket = slinger::Connector::default().connect_with_addr(add).unwrap();
+  socket.set_write_timeout(Some(timeout)).unwrap();
+  socket.set_read_timeout(Some(timeout)).unwrap();
   socket.write_all(b"\r\n").unwrap();
+  socket.flush().unwrap();
+  let mut buf = [0u8; 1];
+  let mut full = Vec::new();
+  while let Ok(size) = socket.read(&mut buf) {
+    if size == 0 {
+      break;
+    }
+    full.extend(buf);
+  }
+  println!("{:?}", String::from_utf8_lossy(&full));
+  socket.shutdown(Shutdown::Both).unwrap();
+}
+
+fn redis() {
+  let add = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6379);
+  let timeout = Duration::new(3, 0);
+  let mut socket = slinger::Connector::default().connect_with_addr(add).unwrap();
+  socket.set_write_timeout(Some(timeout)).unwrap();
+  socket.set_read_timeout(Some(timeout)).unwrap();
+  socket.write_all(b"*1\r\n$4\r\ninfo\r\n").unwrap();
   socket.flush().unwrap();
   let mut buf = [0u8; 1];
   let mut full = Vec::new();
