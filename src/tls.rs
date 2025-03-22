824693c9dba78f84d87c5706676053a794b4742c
cn-kali-team <kali-team@qq.com>
2025-03-22 20:18:38 +0800

    fix

--- src/tls.rs
+++ src/tls.rs
@@ -180,7 +180,7 @@ impl Identity {
   ///
   /// # Optional
   ///
-  /// This requires the `rustls-tls(-...)` Cargo feature enabled.
+  /// This requires the `tls(-...)` Cargo feature enabled.
 
   pub fn from_pem(buf: &[u8]) -> crate::Result<Identity> {
     use rustls_pemfile::Item;
